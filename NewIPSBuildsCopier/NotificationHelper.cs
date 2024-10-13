ef0c1e23350597a3e59f6c1b1d0b7f3bd73ccd02
awnalchuk <awnalchuk@gmail.com>
2024-10-13 19:25:15 +0300

    Добавил методы отображающие уведомления и пишущие лог в класс NotificationHelper.

--- NewIPSBuildsCopier/NotificationHelper.cs
+++ NewIPSBuildsCopier/NotificationHelper.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Toolkit.Uwp.Notifications;
+using Serilog;
 
 namespace IPSBuildsCopier
 {
@@ -15,7 +16,8 @@ namespace IPSBuildsCopier
         public static void ShowNotification(string title, string message)
         {
             // Получаем путь к иконке, которая будет отображаться в уведомлении
-            string iconPath = Path.Combine(Directory.GetCurrentDirectory(), "copy.png");
+            //string iconPath = Path.Combine(Directory.GetCurrentDirectory(), "copy.png");
+            string iconPath = Path.Combine(PathHelper.GetExecutableDirectory().FullName, "copy.png");
 
             // Создаем и настраиваем уведомление
             new ToastContentBuilder()
@@ -23,6 +25,27 @@ namespace IPSBuildsCopier
                 .AddText(message) // Добавляем сообщение
                 .AddAppLogoOverride(new Uri(iconPath)) // Устанавливаем иконку
                 .Show(); // Отображаем уведомление
+        }
+
+        // Информационное уведомление
+        public static void ShowInfoNotificationAndLog(string title, string message)
+        {
+            ShowNotification(title,message);
+            Log.Information(message);
+        }
+
+        // Уведомление о предупреждении
+        public static void ShowWarningNotificationAndLog(string title, string message)
+        {
+            ShowNotification(title, message);
+            Log.Warning(message);
+        }
+
+        // Уведомление об ошибке
+        public static void ShowErrorNotificationAndLog(string title, string message, Exception ex)
+        {
+            ShowNotification(title, message);
+            Log.Warning(ex, message);
         }
     }
 }
